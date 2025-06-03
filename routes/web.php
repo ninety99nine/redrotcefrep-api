3bd34feaecc73cf23f95e0195c927a74aabac28e
Julian Tabona <brandontabona@gmail.com>
2025-06-03 14:41:24 +0200

    Update 03 June 2025 @ 14:41

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,6 @@ use App\Services\Billing\Airtime\OrangeAirtimeService;
 
 Route::get('/test-sms', function () {
 
-
-    $message = 'Welcome! Your order has been confirmed. Track it at example.com/track/12. For help, reply to this SMS or call 0800-456-789. Thank you for shopping with us Today';
-
-    SendSms::dispatch($message, '+26772882239');
-
     $message = 'Welcome! Your order has been confirmed. Track it at example.com/tracking-numbers/123456789/123. For help, reply to this SMS or call 0800-456-789. Thank you for shopping with us Today';
 
     SendSms::dispatch($message, '+26772882239');
